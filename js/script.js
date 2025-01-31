468d76a8c7df8942f5e94e0f2a88575a8b3d02ad
NoeŁ <124210421+Keiroru@users.noreply.github.com>
2025-01-31 14:37:41 +0100

    json

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            function populateTable() {
+                const folders = data.drives.C.folders;
+                const tableBody = document.getElementsByTagName('tbody')[0];
+    
+                folders.forEach(folder => {
+                    const row = document.createElement('tr');
+    
+                    const nameCell = document.createElement('td');
+                    nameCell.textContent = folder.name;
+                    row.appendChild(nameCell);
+    
+                    const extensionCell = document.createElement('td');
+                    extensionCell.textContent = folder.extension;
+                    row.appendChild(extensionCell);
+    
+                    const sizeCell = document.createElement('td');
+                    sizeCell.textContent = folder.size;
+                    row.appendChild(sizeCell);
+    
+                    const dateCell = document.createElement('td');
+                    dateCell.textContent = folder.date;
+                    row.appendChild(dateCell);
+    
+                    tableBody.appendChild(row);
+                });
+            }
+            populateTable();
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+    
+
+
+    
     const resizers = document.querySelectorAll('th .resizer, .middle .resizer');
     let startX, startWidth, resizerParent;
 
@@ -38,18 +75,18 @@ function showDropdown1() {
     document.getElementById("dropdown1").classList.toggle("show");
 }
 
-function lemezchange(lemez,path){
+function lemezchange(lemez, path) {
     let string = ":\\";
-    document.getElementById(path).innerHTML=lemez+string;
+    document.getElementById(path).innerHTML = lemez + string;
 }
 
-function load(nev){
-    let szoveg =  document.getElementById("path").innerHTML;
-    document.getElementById("path").innerHTML=szoveg+nev;
+function load(nev) {
+    let szoveg = document.getElementById("path").innerHTML;
+    document.getElementById("path").innerHTML = szoveg + nev;
     checkfiles();
 }
-function checkfiles(){
-    
+function checkfiles() {
+
 }
 
 
